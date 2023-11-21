e041c67dc65ce7833af52a2456fca135bc3bab3c
MariangelRondan <rondanmariangel@gmail.com>
2023-11-21 18:40:50 -0300

    depurado

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -37,3 +37,47 @@ const getAllTeams = async () => {
 };
 
 module.exports = { getAllTeams };
+
+// const { Team } = require("../db");
+// const axios = require("axios");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const URL = process.env.URL_API;
+
+// const getAllTeams = async () => {
+//   const teamCount = await Team.count();
+
+//   // Si ya hay equipos en la base de datos, devuélvelos directamente
+//   if (teamCount > 0) {
+//     const teamsBDD = await Team.findAll();
+//     return teamsBDD;
+//   } else {
+//     // Si no hay equipos en la base de datos, espera a que se almacenen antes de obtenerlos de la API
+//     const { data } = await axios.get(`${URL}`);
+
+//     const teamsApi = [];
+//     data.forEach((driver) => {
+//       if (driver && driver.teams) {
+//         if (!driver.teams.includes(",")) {
+//           teamsApi.push(driver.teams);
+//         } else {
+//           const teamsArray = driver.teams.split(",").map((team) => team.trim());
+//           teamsApi.push(...teamsArray);
+//         }
+//       }
+//     });
+
+//     const uniqueTeams = [...new Set(teamsApi)];
+//     uniqueTeams.forEach(async (team) => {
+//       await Team.create({
+//         name: team,
+//       });
+//     });
+//     const teamsBDD = await Team.findAll();
+//     return teamsBDD;
+//   }
+//   // Devuelve los equipos recién almacenados en la base de datos
+// };
+
+// module.exports = { getAllTeams };
